c4c1765e8209ee1565e573f78ee94c0bd0a307ac
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-06-12 04:00:04 +0900

    Create main1.dart

---
+++ stop_watch/main1.dart
@@ -0,0 +1,31 @@
+// 데이터가 있냐? -> stateful
+
+import 'package:flutter/material.dart';
+import 'package:woo_f/screens/home_screen.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  // List<int> numbers = [];
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+          ),
+        ),
+        cardColor: const Color(0xFFF4EDDB),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
