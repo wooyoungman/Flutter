9ecf12dcd99087c9d25ec6c51f12ffddb31f3607
정우영 <101693311+wooyoungman@users.noreply.github.com>
2023-06-12 04:01:58 +0900

    Create main2.dart

---
+++ today's_webtoon/main2.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:woo_f/screens/home_screen2.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: HomeScreen2(),
+    );
+  }
+}
